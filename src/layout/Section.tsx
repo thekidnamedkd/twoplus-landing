861365960a069750715220f09a998ebb8091f4de
Kevin Davis <thekidnamedkd@gmail.com>
2022-04-21 16:39:20 +0200

    more styling changes to layout

--- src/layout/Section.tsx
+++ src/layout/Section.tsx
@@ -9,7 +9,7 @@ type ISectionProps = {
 
 const Section = (props: ISectionProps) => (
   <div
-    className={`max-w-screen-lg mx-auto px-3 ${
+    className={`max-w-screen-lg max-h-auto mx-auto px-3 ${
       props.yPadding ? props.yPadding : 'py-16'
     }`}
   >
