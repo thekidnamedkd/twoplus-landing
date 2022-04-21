e46296d54f7857fe9a8ce9e0fb05ded6fd685b50
Kevin Davis <65736142+thekidnamedkd@users.noreply.github.com>
2022-04-21 16:59:37 +0200

    Revert "more styling changes to layout"

--- src/navigation/NavbarTwoColumns.tsx
+++ src/navigation/NavbarTwoColumns.tsx
@@ -13,7 +13,7 @@ const NavbarTwoColumns = (props: INavbarProps) => (
         <a>{props.logo}</a>
       </Link>
     </div>
-    <button className="bg-tpBlack-base ml-5 mt-5 md:m-0 px-5 py-5 font-bold text-md transition-colors duration-300 border rounded-lg shadow-[0px_0px_10px_3px] text-tpCream-base border-tpBerry-base shadow-tpBerry-base  hover:bg-tpBerry-base  hover:text-tpBlack-base">
+    <button className="bg-tpBlack-base ml-5 mt-10 md:m-0 px-5 py-5 font-bold text-md transition-colors duration-300 border rounded-lg shadow-[0px_0px_10px_3px] text-tpCream-base border-tpBerry-base shadow-tpBerry-base  hover:bg-tpBerry-base  hover:text-tpBlack-base">
       <Link href="https://jv2bphd1l4e.typeform.com/to/J24VtRtW">
         <a target="_blank">Register Interest</a>
       </Link>
